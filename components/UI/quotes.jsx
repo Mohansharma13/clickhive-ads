57493769ec73c9a5db7c47f58c4d61809ea43f3a
Mohan Sharma <msharma13613@gmail.com>
2025-04-23 18:59:05 +0530

    change quotes image

--- components/UI/quotes.jsx
+++ components/UI/quotes.jsx
@@ -5,22 +5,18 @@ const quotes = [
   {
     text: "Creativity without strategy is called 'art.' Creativity with strategy is called 'advertising.'",
     author: "Jef I. Richards",
-    image: "https://i.pravatar.cc/100?img=1"
   },
   {
     text: "Marketing is no longer about the stuff that you make, but about the stories you tell.",
     author: "Seth Godin",
-    image: "https://i.pravatar.cc/100?img=5"
   },
   {
     text: "The best marketing doesn’t feel like marketing.",
     author: "Tom Fishburne",
-    image: "https://i.pravatar.cc/100?img=3"
   },
   {
     text: "Content is fire. Social media is gasoline.",
     author: "Jay Baer",
-    image: "https://i.pravatar.cc/100?img=9"
   }
 ];
 
@@ -37,9 +33,6 @@ const FancyQuoteCarousel = () => {
   return (
     <div className="fancy-carousel">
       <div className="quote-card">
-        <div className="quote-img">
-          <img src={quotes[index].image} alt={quotes[index].author} />
-        </div>
         <div className="quote-content fade">
           <p className="quote-text">“{quotes[index].text}”</p>
           <p className="quote-author">– {quotes[index].author}</p>
